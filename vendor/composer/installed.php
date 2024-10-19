8f003c98e4de523230c6394c94c4f0ab100dbaaf
premod <premodsuraweera1@gmail.com>
2024-10-19 23:56:20 +0530

    Add "lopzy/contact" package and update composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lopzy/contact',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '855f4ebd270f83baf54acfb3662201b455cd5dc6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lopzy/contact' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '855f4ebd270f83baf54acfb3662201b455cd5dc6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
