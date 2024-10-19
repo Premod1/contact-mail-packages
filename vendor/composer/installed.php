ea90efca91c405bf362a8d40f22dcfb3fd3bedb6
premod <premodsuraweera1@gmail.com>
2024-10-20 00:30:13 +0530

    Update reference to 'lopzy/contact' package in composer.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'lopzy/contact',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '004217201d75c41124573cabdeba10d279402fe3',
+        'reference' => '41f0925fa7800200ac645f0da51dc5fa451c1761',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'lopzy/contact' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '004217201d75c41124573cabdeba10d279402fe3',
+            'reference' => '41f0925fa7800200ac645f0da51dc5fa451c1761',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
